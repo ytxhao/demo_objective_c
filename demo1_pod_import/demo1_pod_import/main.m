1748ae047451332f0492213819691c1bb65f58bb
hao.yu <hao.yu@ushow.media>
2019-07-25 17:06:35 +0800

    create project

---
+++ demo1_pod_import/demo1_pod_import/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  demo1_pod_import
+//
+//  Created by yuhao on 2019/7/25.
+//  Copyright © 2019 yuhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
